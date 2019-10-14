8b7f9c6eb69092179fa14febbd60f824688eeac1
jayvee-fullstack <jayvee@fullstack.ph>
2019-10-14 20:05:22 +0800

    Calendar: Day/Week/Month view

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import React from 'react';
+import React, { useState, createRef } from 'react';
 import './App.css';
 import Calendar from '@toast-ui/react-calendar'
 import 'tui-calendar/dist/tui-calendar.css'
 
 function App() {
+  const [currView, setCurrView] = useState('month')
+  const calendarRef = createRef()
+
+  const handleCurrView = view => {
+    setCurrView(view)
+  }
+
+  const view = [
+    'day',
+    'week',
+    'month'
+  ]
+
+  const calendarOptions = {
+    usageStatistics: false,
+    taskView: false,
+    scheduleView: true,
+    view: currView,
+    disableDblClick: true,
+    disableClick: false,
+    useDetailPopup: true,
+    useCreationPopup: true
+  }
+
+  const handlePrevButton = () => {
+    const calendarInstance = calendarRef.current.getInstance()
+
+    calendarInstance.prev()
+  }
+
+  const handleNextButton = () => {
+    const calendarInstance = calendarRef.current.getInstance()
+
+    calendarInstance.next()
+  }
+
   return (
     <div className="App">
+      <div>
+        {view.map(item => (
+          <button key={item} onClick={() => handleCurrView(item)}>{item}</button>
+        ))}
+      </div>
+      <div>
+        <button onClick={handlePrevButton}>Prev</button>
+        <button onClick={handleNextButton}>Next</button>
+      </div>
       <Calendar
-        usageStatistics={false}
-        view='month'
-        height="99.5vh"
-        taskView={false}
-        disableDblClick={true}
-        disableClick={false}
-        isReadOnly={false}
-        month={{
-          startDayOfWeek: 0
-        }}
+        ref={calendarRef}
+        {...calendarOptions}
       />
     </div>
   );
